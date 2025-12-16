5befe0c0993102e5eb7bf135730d7657cfa94e42
sijun-yang <yangsijun5528@gmail.com>
2025-12-17 00:36:24 +0900

    style: update code style

--- fesod/src/test/java/org/apache/fesod/sheet/converter/ConverterIsolationTest.java
+++ fesod/src/test/java/org/apache/fesod/sheet/converter/ConverterIsolationTest.java
@@ -1,9 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.fesod.sheet.converter;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
-
 import org.apache.fesod.sheet.ExcelWriter;
 import org.apache.fesod.sheet.FesodSheet;
 import org.apache.fesod.sheet.converters.Converter;
@@ -16,8 +33,7 @@ import org.junit.jupiter.api.Test;
 
 public class ConverterIsolationTest {
 
-    public static class TestData {
-    }
+    public static class TestData {}
 
     public static class ConverterA implements Converter<String> {
 
@@ -39,20 +55,14 @@ public class ConverterIsolationTest {
 
     @Test
     public void testConverterIsolation() {
-        ExcelWriter writer1 = FesodSheet
-                .write(new File(TestFileUtil.getPath() + "writer1.xlsx"), TestData.class)
+        ExcelWriter writer1 = FesodSheet.write(new File(TestFileUtil.getPath() + "writer1.xlsx"), TestData.class)
                 .registerConverter(new ConverterA())
                 .build();
 
-        ExcelWriter writer2 = FesodSheet
-                .write(new File(TestFileUtil.getPath() + "writer2.xlsx"), TestData.class)
+        ExcelWriter writer2 = FesodSheet.write(new File(TestFileUtil.getPath() + "writer2.xlsx"), TestData.class)
                 .build();
 
-        boolean writer2HasConverterA = writer2.writeContext()
-                .currentWriteHolder()
-                .converterMap()
-                .values()
-                .stream()
+        boolean writer2HasConverterA = writer2.writeContext().currentWriteHolder().converterMap().values().stream()
                 .anyMatch(c -> c instanceof ConverterA);
 
         writer1.finish();
